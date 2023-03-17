90de9479c2bf18d640a3cb0c103bf1e5825fcb80
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2023-03-17 16:51:52 +0800

    ast2700: Add SPL initialization and booting device selection
    Add init. and booting-device selection for SPL booting.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: I2b972d8b10482c99d3ef1a0077d36a4c5f961f1c

--- arch/arm/mach-aspeed/ast2700/spl.c
+++ arch/arm/mach-aspeed/ast2700/spl.c
@@ -17,6 +17,24 @@ DECLARE_GLOBAL_DATA_PTR;
 
 void board_init_f(ulong dummy)
 {
+	if (CONFIG_IS_ENABLED(OF_CONTROL)) {
+		int ret;
+
+		ret = spl_early_init();
+		if (ret) {
+			debug("spl_early_init() failed: %d\n", ret);
+			hang();
+		}
+	}
+
+	preloader_console_init();
+
+	dram_init();
+}
+
+struct legacy_img_hdr *spl_get_load_buffer(ssize_t offset, size_t size)
+{
+	return (struct legacy_img_hdr *)(CONFIG_SYS_LOAD_ADDR);
 }
 
 /*
@@ -25,5 +43,5 @@ void board_init_f(ulong dummy)
  */
 u32 spl_boot_device(void)
 {
-	return 0;
+	return BOOT_DEVICE_RAM;
 }
