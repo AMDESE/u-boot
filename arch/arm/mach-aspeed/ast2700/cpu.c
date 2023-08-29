23f1b13ede6ff4148f73328acf44580cf3837c0e
Kevin Chen <kevin_chen@aspeedtech.com>
2023-08-29 19:16:54 +0800

    arch: arm: Refine MMU table for all existed memory.
    0x0~0x3_0000_0000(12GB): device memory with read/write/execute.
    0x4_0000_0000~0x6_0000_0000(8GB): normal memory by DRAM capacity.
    
    Change-Id: Ie09f4e039d9373d8777cba7b547dca1485cd6285
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- arch/arm/mach-aspeed/ast2700/cpu.c
+++ arch/arm/mach-aspeed/ast2700/cpu.c
@@ -14,22 +14,14 @@ static struct mm_region aspeed2700_mem_map[] = {
 		{
 				.virt = 0x0UL,
 				.phys = 0x0UL,
-				.size = 0x40000000UL,
-				.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
-					 PTE_BLOCK_NON_SHARE |
-					 PTE_BLOCK_PXN | PTE_BLOCK_UXN
-		},
-		{
-				.virt = 0x100000000UL,
-				.phys = 0x100000000UL,
-				.size =  0x40000000UL,
+				.size = 0x300000000UL,
 				.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 					 PTE_BLOCK_NON_SHARE
 		},
 		{
 				.virt = 0x400000000UL,
 				.phys = 0x400000000UL,
-				.size =  0x40000000UL,
+				.size = 0x200000000UL,
 				.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 					 PTE_BLOCK_INNER_SHARE
 		},
