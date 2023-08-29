f17637a8f2a4b4e921597bbb88633005bc5239a8
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2023-08-29 16:38:07 +0800

    ast2600: Update flash layout
    1. Increase SPL/U-Boot flash region size
    2. update ENV offset.
    3. update boot command to locate kernel FIT image
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: Ieea0842cd4c669cf56072e3f0a34048d5715bf1b

--- include/configs/evb_ast2600.h
+++ include/configs/evb_ast2600.h
@@ -16,13 +16,9 @@
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"bootspi=fdt addr 20100000 && fdt header get fitsize totalsize && " \
-	"cp.b 20100000 ${loadaddr} ${fitsize} && bootm; " \
+	"bootspi=fdt addr 20160000 && fdt header get fitsize totalsize && " \
+	"cp.b 20160000 ${loadaddr} ${fitsize} && bootm; " \
 	"echo Error loading kernel FIT image\0" \
 	""
-
-#if CONFIG_IS_ENABLED(OPTEE_IMAGE)
-#define CONFIG_ARMV7_SECURE_BASE	0x88000000
-#endif
 
 #endif	/* __CONFIG_H */
