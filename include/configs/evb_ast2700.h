ec1c76579cf7a833b402e3f9dfb8c7a47bacf625
Ryan Chen <ryan_chen@aspeedtech.com>
2023-09-20 09:04:38 +0800

    evb_ast2600: add emmc env
    for consistence emmc spi boot env
    
    Signed-off-by: Ryan Chen <ryan_chen@aspeedtech.com>
    Change-Id: I541501c72dd049b5f91eda63fce7850733cc9588

--- include/configs/evb_ast2700.h
+++ include/configs/evb_ast2700.h
@@ -8,6 +8,10 @@
 
 #include <configs/aspeed-common.h>
 
+/* Extra ENV for Boot Command */
+#define STR_HELPER(n)	#n
+#define STR(n)		STR_HELPER(n)
+
 #define CONFIG_SYS_MEMTEST_START	(CONFIG_SYS_SDRAM_BASE + 0x300000)
 #define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x5000000)
 
@@ -17,6 +21,13 @@
 	"bootspi=fdt addr 1002a0000 && fdt header get fitsize totalsize && " \
 	"cp.b 1002a0000 ${loadaddr} ${fitsize} && bootm; " \
 	"echo Error loading kernel FIT image\0" \
+	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0"	\
+	"bootside=a\0"	\
+	"rootfs=rofs-a\0"	\
+	"setmmcargs=setenv bootargs ${bootargs} rootwait root=PARTLABEL=${rootfs}\0"	\
+	"boota=setenv bootpart 2; setenv rootfs rofs-a; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm; echo Error loading kernel FIT image\0"	\
+	"bootb=setenv bootpart 3; setenv rootfs rofs-b; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm; echo Error loading kernel FIT image\0"	\
+	"bootmmc=if test \"${bootside}\" = \"b\"; then run bootb; run boota; else run boota; run bootb; fi\0"	\
+	"verify=yes\0"	\
 	""
-
 #endif	/* __CONFIG_H */
