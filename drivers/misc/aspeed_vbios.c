ed118e8f2c122e3c3cb73d6a964097ddb7c85dc9
Tommy Huang <tommy_huang@aspeedtech.com>
2023-10-27 19:21:35 +0800

    drivers: misc: Update pcie1 vbios applied flow
    Add chip reversion / pcie enable checked flow for applied vbios.
    Update pcie1 vbios applied flow.
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: Ic179a00c29514e995db705b279b70b2e903e4f1b

--- drivers/misc/aspeed_vbios.c
+++ drivers/misc/aspeed_vbios.c
@@ -16,41 +16,124 @@
 #include <asm/io.h>
 #include "ast_vbios.h"
 
-#define VBIOS0_SCU_OFFSET 0xa2c
+#define VBIOS0_SCU_OFFSET	(0xa2c)
+#define VBIOS1_SCU_OFFSET	(0xaac)
+#define SCU_PCI_MISC70		(0xa70)
+#define SCU_PCI_MISCF0		(0xaf0)
+#define SCU_REVISION		(0x0)
+#define SCU_REVISION_ID_EFUSE	GENMASK(15, 8)
+#define EFUSE_AST2700		(0x1)
+#define EFUSE_AST2720		(0x2)
 
 struct aspeed_vbios_priv {
 	struct regmap *scu_ctl_base;
 	void *e2m0_ctl_base;
+	void *e2m1_ctl_base;
 	void *vbios0_base;
+	void *vbios1_base;
 };
 
 static int aspeed_vbios_probe(struct udevice *dev)
 {
 	struct aspeed_vbios_priv *vbios = dev_get_priv(dev);
-	u32 vbios0_e2m_value;
-	u64 vbios0_mem_base;
-	u32 vbios0_mem_length;
-	u64 temp;
+	u32 value, efuse;
+	bool is_pcie0_enable, is_pcie1_enable;
+	u32 vbios_e2m_value;
+	u64 vbios_mem_base;
+	u32 vbios_mem_length;
+	struct fdt_resource res;
 
-	temp = (uintptr_t)(vbios->scu_ctl_base);
-	dev_err(dev, "scu_ctl_base %llx\n", temp);
+	/* Get chip version */
+	regmap_read(vbios->scu_ctl_base, SCU_REVISION, &value);
+	efuse = FIELD_GET(SCU_REVISION_ID_EFUSE, value);
 
-	/* Get the controller base address */
-	vbios0_mem_base = (uintptr_t)(vbios->vbios0_base);
-	vbios0_mem_length = sizeof(uefi);
+	/* Get PCIE status */
+	regmap_read(vbios->scu_ctl_base, SCU_PCI_MISC70, &value);
+	is_pcie0_enable = value & BIT(0);
 
-	memcpy((u32 *)vbios->vbios0_base, uefi, vbios0_mem_length);
-	invalidate_dcache_range(vbios0_mem_base, vbios0_mem_base
-	+ vbios0_mem_length);
+	regmap_read(vbios->scu_ctl_base, SCU_PCI_MISCF0, &value);
+	is_pcie1_enable = value & BIT(0);
 
-	/* Set VBIOS 32KB into reserved buffer */
-	vbios0_e2m_value = (vbios0_mem_base >> 4) | 0x04;
+	if (efuse == EFUSE_AST2700) {
+		/* single display */
+		is_pcie1_enable = 0;
+	} else if (efuse == EFUSE_AST2720) {
+		/* without display*/
+		return -1;
+	}
 
-	/* Set VBIOS setting into e2m */
-	writel(vbios0_e2m_value, vbios->e2m0_ctl_base + 0x4);
+	/* load vbios for pcie 0 node */
+	if (is_pcie0_enable) {
+		/* obtain the vbios0 reserved memory */
+		value = fdt_path_offset(gd->fdt_blob, "/reserved-memory/pcie_vbios0");
+		fdt_get_resource(gd->fdt_blob, value, "reg", 0, &res);
+		vbios->vbios0_base = (void *)res.start;
+		if (IS_ERR(vbios->vbios0_base)) {
+			dev_err(dev, "can't obtain pcie_vbios0 reserved\n");
+			return PTR_ERR(vbios->vbios0_base);
+		}
 
-	/* Set VBIOS setting into scu */
-	regmap_write(vbios->scu_ctl_base, VBIOS0_SCU_OFFSET, vbios0_e2m_value);
+		/* Get the controller base address */
+		vbios_mem_base = (uintptr_t)(vbios->vbios0_base);
+		vbios_mem_length = sizeof(uefi);
+
+		/* Initial memory region and copy vbios into it */
+		memset((u32 *)vbios->vbios0_base, 0x0, 0x10000);
+		memcpy((u32 *)vbios->vbios0_base, uefi, vbios_mem_length);
+		invalidate_dcache_range(vbios_mem_base, vbios_mem_base
+		+ 0x10000);
+
+		/* Set VBIOS 64KB into reserved buffer */
+		vbios_e2m_value = (vbios_mem_base >> 4) | 0x05;
+
+		/* Set VBIOS setting into e2m */
+		writel(vbios_e2m_value, vbios->e2m0_ctl_base + 0x4);
+
+		/* Set VBIOS setting into scu */
+		regmap_write(vbios->scu_ctl_base, VBIOS0_SCU_OFFSET, vbios_e2m_value);
+	} else {
+		/* clear VBIOS setting into e2m */
+		writel(0, vbios->e2m0_ctl_base + 0x4);
+
+		/* clear VBIOS setting into scu */
+		regmap_write(vbios->scu_ctl_base, VBIOS0_SCU_OFFSET, 0);
+	}
+
+	/* load vbios for pcie 1 node */
+	if (is_pcie1_enable) {
+		/* obtain the vbios0 reserved memory */
+		value = fdt_path_offset(gd->fdt_blob, "/reserved-memory/pcie_vbios1");
+		fdt_get_resource(gd->fdt_blob, value, "reg", 0, &res);
+		vbios->vbios1_base = (void *)res.start;
+		if (IS_ERR(vbios->vbios1_base)) {
+			dev_err(dev, "can't obtain pcie_vbios1 reserved\n");
+			return PTR_ERR(vbios->vbios1_base);
+		}
+
+		/* Get the controller base address */
+		vbios_mem_base = (uintptr_t)(vbios->vbios1_base);
+		vbios_mem_length = sizeof(uefi);
+
+		memset((u32 *)vbios->vbios1_base, 0x0, 0x10000);
+		memcpy((u32 *)vbios->vbios1_base, uefi, vbios_mem_length);
+		invalidate_dcache_range(vbios_mem_base, vbios_mem_base
+		+ 0x10000);
+
+		/* Set VBIOS 64KB into reserved buffer */
+		vbios_e2m_value = (vbios_mem_base >> 4) | 0x05;
+
+		/* Set VBIOS setting into e2m */
+		writel(vbios_e2m_value, vbios->e2m1_ctl_base + 0x4);
+
+		/* Set VBIOS setting into scu */
+		regmap_write(vbios->scu_ctl_base, VBIOS1_SCU_OFFSET, vbios_e2m_value);
+	} else {
+		/* clear VBIOS1 setting into e2m */
+		writel(0, vbios->e2m1_ctl_base + 0x4);
+
+		/* clear VBIOS1 setting into scu */
+		regmap_write(vbios->scu_ctl_base, VBIOS1_SCU_OFFSET, 0);
+	}
 
 	return 0;
 }
@@ -58,8 +141,6 @@ static int aspeed_vbios_probe(struct udevice *dev)
 static int aspeed_vbios_of_to_plat(struct udevice *dev)
 {
 	struct aspeed_vbios_priv *vbios = dev_get_priv(dev);
-	struct fdt_resource res;
-	u32 nodeoff;
 	/* Get the controller base address */
 
 	vbios->e2m0_ctl_base = (void *)devfdt_get_addr_index(dev, 0);
@@ -68,19 +149,18 @@ static int aspeed_vbios_of_to_plat(struct udevice *dev)
 		return PTR_ERR(vbios->e2m0_ctl_base);
 	}
 
+	vbios->e2m1_ctl_base = (void *)devfdt_get_addr_index(dev, 1);
+	if (IS_ERR(vbios->e2m1_ctl_base)) {
+		dev_err(dev, "can't allocate e2m1_ctl\n");
+		return PTR_ERR(vbios->e2m1_ctl_base);
+	}
+
 	vbios->scu_ctl_base = syscon_regmap_lookup_by_phandle(dev, "aspeed,scu");
 	if (IS_ERR(vbios->scu_ctl_base)) {
-		dev_err(dev, "can't get regmap for scu!\n");
+		dev_err(dev, "can't allocate scu_ctl\n");
 		return PTR_ERR(vbios->scu_ctl_base);
 	}
 
-	nodeoff = fdt_path_offset(gd->fdt_blob, "/reserved-memory/pcie_vbios0");
-	fdt_get_resource(gd->fdt_blob, nodeoff, "reg", 0, &res);
-	vbios->vbios0_base = (void *)res.start;
-	if (IS_ERR(vbios->vbios0_base)) {
-		dev_err(dev, "can't obtain pcie_vbios0\n");
-		return PTR_ERR(vbios->vbios0_base);
-	}
 	return 0;
 }
 
