78b6d4e46c3f078baf16c4afd16b0ece2037a06e
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2023-11-27 17:22:00 +0800

    misc: aspeed_edaf_bridge: Add more configuration
    Add more configuration for eDAF bridge to access.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: I88a910252f604627d19a955db0a24ebb267d3985

--- drivers/misc/aspeed_edaf_bridge.c
+++ drivers/misc/aspeed_edaf_bridge.c
@@ -3,29 +3,38 @@
  * Copyright (C) ASPEED Technology Inc.
  */
 
+#include <asm/io.h>
 #include <common.h>
 #include <clk.h>
 #include <dm.h>
 #include <errno.h>
-#include <reset.h>
 #include <fdtdec.h>
-#include <asm/io.h>
+#include <linux/bitfield.h>
+#include <reset.h>
 
+#define EDAF_BDGE_CBASE			0x20
+#define EDAF_BDGE_MBASE			0x24
 #define EDAF_BDGE_CMD_READ		0x40
+#define EDAF_BDGE_CMD_WRITE_EN		0x44
 #define EDAF_BDGE_CMD_WRITE		0x48
-#define EDAF_BDGE_CMD_ERASE_4K	0x50
-#define EDAF_BDGE_CMD_ERASE_32K	0x54
-#define EDAF_BDGE_CMD_ERASE_64K	0x58
+#define EDAF_BDGE_CMD_READ_STS		0x4c
+#define EDAF_BDGE_CMD_ERASE_4K		0x50
+#define EDAF_BDGE_CMD_ERASE_32K		0x54
+#define EDAF_BDGE_CMD_ERASE_64K		0x58
+#define EDAF_BDGE_MISC			0x70
+#define   EDAF_BDGE_MISC_MBASE_H	GENMASK(15, 8)
+#define   EDAF_BDGE_MISC_CBASE_H	GENMASK(7, 0)
 
 static int aspeed_edaf_bridge_probe(struct udevice *dev)
 {
-	void *edaf_bridge_base;
+	void *edaf_bridge_regs;
+	uint64_t cbase, mbase;
 	uint32_t cfg;
 	ofnode node;
 	int rc;
 
-	edaf_bridge_base = (void *)devfdt_get_addr_index(dev, 0);
-	if (edaf_bridge_base == (void *)FDT_ADDR_T_NONE) {
+	edaf_bridge_regs = (void *)devfdt_get_addr_index(dev, 0);
+	if (edaf_bridge_regs == (void *)FDT_ADDR_T_NONE) {
 		printf("cannot get eDAF MCU base\n");
 		return -ENODEV;
 	};
@@ -36,25 +45,53 @@ static int aspeed_edaf_bridge_probe(struct udevice *dev)
 		return -ENODEV;
 	}
 
+	rc = ofnode_read_u64(node, "ctl-base", &cbase);
+	if (!rc) {
+		writel(cbase, edaf_bridge_regs + EDAF_BDGE_CBASE);
+
+		cfg = readl(edaf_bridge_regs + EDAF_BDGE_MISC);
+		cfg &= ~EDAF_BDGE_MISC_CBASE_H;
+		cfg |= FIELD_PREP(EDAF_BDGE_MISC_CBASE_H, cbase >> 32);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_MISC);
+	}
+
+	rc = ofnode_read_u64(node, "mem-base", &mbase);
+	if (!rc) {
+		writel(mbase, edaf_bridge_regs + EDAF_BDGE_MBASE);
+
+		cfg = readl(edaf_bridge_regs + EDAF_BDGE_MISC);
+		cfg &= ~EDAF_BDGE_MISC_MBASE_H;
+		cfg |= FIELD_PREP(EDAF_BDGE_MISC_MBASE_H, mbase >> 32);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_MISC);
+	}
+
 	rc = ofnode_read_u32(node, "cmd-read", &cfg);
 	if (!rc)
-		writel(cfg, edaf_bridge_base + EDAF_BDGE_CMD_READ);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_READ);
+
+	rc = ofnode_read_u32(node, "cmd-write-enable", &cfg);
+	if (!rc)
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_WRITE_EN);
 
 	rc = ofnode_read_u32(node, "cmd-write", &cfg);
 	if (!rc)
-		writel(cfg, edaf_bridge_base + EDAF_BDGE_CMD_WRITE);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_WRITE);
+
+	rc = ofnode_read_u32(node, "cmd-read-status", &cfg);
+	if (!rc)
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_READ_STS);
 
 	rc = ofnode_read_u32(node, "cmd-erase-4k", &cfg);
 	if (!rc)
-		writel(cfg, edaf_bridge_base + EDAF_BDGE_CMD_ERASE_4K);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_ERASE_4K);
 
 	rc = ofnode_read_u32(node, "cmd-erase-32k", &cfg);
 	if (!rc)
-		writel(cfg, edaf_bridge_base + EDAF_BDGE_CMD_ERASE_32K);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_ERASE_32K);
 
 	rc = ofnode_read_u32(node, "cmd-erase-64k", &cfg);
 	if (!rc)
-		writel(cfg, edaf_bridge_base + EDAF_BDGE_CMD_ERASE_64K);
+		writel(cfg, edaf_bridge_regs + EDAF_BDGE_CMD_ERASE_64K);
 
 	return 0;
 }
