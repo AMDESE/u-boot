7666797d921541bfc198d428d53d2e4654b896af
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2024-01-26 15:21:42 +0800

    arm: aspeed: Support SPI DMA length greater than 32MB
    If the total DMA length is greater than 32MB, divide it
    into individual parts and execute DMA operation separately.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: Ifde5682a11ee4d52ed871a2569dad4c0bf202d57

--- arch/arm/mach-aspeed/ast2700/spi_dma.c
+++ arch/arm/mach-aspeed/ast2700/spi_dma.c
@@ -14,41 +14,56 @@
 #define DMA_RAM_ADDR	(ASPEED_FMC_REG_BASE + 0x088)
 #define DMA_LEN		(ASPEED_FMC_REG_BASE + 0x08C)
 
+#define SPI_DMA_MAX_LEN			(32 * 1024 * 1024)
+
 #define SPI_DMA_DONE			BIT(11)
 #define DMA_ENABLE			BIT(0)
 
 size_t aspeed_memmove_dma_op(void *dest, const void *src, size_t count)
 {
 	u32 dma_busy = 0;
+	u64 dma_dest = (u64)dest;
+	u64 dma_src = (u64)src;
+	u32 remaining = (u32)count;
+	u32 dma_len = (u32)count;
 
-	if (dest == src)
+	if (dma_dest == dma_src)
 		return 0;
 
-	if ((u64)dest % 4 != 0 || (u64)src % 4 != 0)
+	if (dma_dest % 4 != 0 || dma_src % 4 != 0)
 		return 0;
 
-	if ((u64)src >= ASPEED_FMC_CS0_BASE &&
-	    (u64)src < (ASPEED_FMC_CS0_BASE + ASPEED_FMC_CS0_SIZE) &&
-	    (u64)dest >= ASPEED_DRAM_BASE) {
-		if ((u64)dest >= ASPEED_DRAM_BASE)
-			writel(0x4, (void *)DRAM_HI_ADDR);
+	if (dma_src >= ASPEED_FMC_CS0_BASE &&
+	    dma_src + dma_len < (ASPEED_FMC_CS0_BASE + ASPEED_FMC_CS0_SIZE) &&
+	    dma_dest >= ASPEED_DRAM_BASE) {
+		while (remaining > 0) {
+			if (dma_len > SPI_DMA_MAX_LEN)
+				dma_len = SPI_DMA_MAX_LEN;
+			else
+				dma_len = remaining;
 
-		writel((u32)((u64)dest - ASPEED_DRAM_BASE),
-		       (void *)DMA_RAM_ADDR);
-		writel((u32)((u64)src - ASPEED_FMC_CS0_BASE),
-		       (void *)DMA_FLASH_ADDR);
-		writel(count - 1, (void *)DMA_LEN);
+			if (dma_dest >= ASPEED_DRAM_BASE)
+				writel(0x4, (void *)DRAM_HI_ADDR);
 
-		writel(DMA_ENABLE, (void *)DMA_CTRL);
+			writel((u32)(dma_dest - ASPEED_DRAM_BASE),
+			       (void *)DMA_RAM_ADDR);
+			writel((u32)(dma_src - ASPEED_FMC_CS0_BASE),
+			       (void *)DMA_FLASH_ADDR);
+			writel(dma_len - 1, (void *)DMA_LEN);
 
-		do {
-			dma_busy = readl((void *)INTR_CTRL) & SPI_DMA_DONE;
-		} while (dma_busy == 0);
+			writel(DMA_ENABLE, (void *)DMA_CTRL);
 
-		writel(0x0, (void *)DMA_CTRL);
+			do {
+				dma_busy = readl((void *)INTR_CTRL) & SPI_DMA_DONE;
+			} while (dma_busy == 0);
+
+			writel(0x0, (void *)DMA_CTRL);
+			dma_dest += dma_len;
+			dma_src += dma_len;
+			remaining -= dma_len;
+		}
 
 		return count;
 	}
-
 	return 0;
 }
