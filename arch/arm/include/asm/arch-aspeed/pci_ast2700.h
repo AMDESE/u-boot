bda39cb363d3bb8b49b7f7f9df11d99e6df6eb1b
Jammy Huang <jammy_huang@aspeedtech.com>
2024-03-05 08:47:00 +0800

    arm: mach-aspeed: ast2700: Separate pci from platform
    Create pci for related tasks. Thus, pci_vga_init are moved from platform
    to here.
    
    Change-Id: If2196ffefe3db87e3da5a552eb6ad14146ef44c8
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>

---
+++ arch/arm/include/asm/arch-aspeed/pci_ast2700.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) Aspeed Technology Inc.
+ */
+#ifndef _ASM_ARCH_PCI_AST2700_H
+#define _ASM_ARCH_PCI_AST2700_H
+
+#include <asm/arch-aspeed/scu_ast2700.h>
+
+void pci_config(struct ast2700_soc0_scu *scu);
+
+#endif
