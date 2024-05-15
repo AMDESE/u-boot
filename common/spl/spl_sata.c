1248b4b6add5adb81d2fe27a45ffbe06e3cf133e
Cool Lee <cool_lee@aspeedtech.com>
2024-05-15 10:39:42 +0800

    spl: sata: add load fit
    UFS boot is followed on the spl_sata framework.
    
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: I93629579ef50753a02dc6da17aa9a43903dd334f

--- common/spl/spl_sata.c
+++ common/spl/spl_sata.c
@@ -17,21 +17,16 @@
 #include <fat.h>
 #include <image.h>
 
-static int spl_sata_load_image_raw(struct spl_image_info *spl_image,
-		struct spl_boot_device *bootdev,
-		struct blk_desc *stor_dev, unsigned long sector)
+static int sata_load_legacy(struct spl_image_info *spl_image,
+			    struct spl_boot_device *bootdev,
+			    struct blk_desc *stor_dev,
+			    ulong sector, struct legacy_img_hdr *header)
 {
-	struct legacy_img_hdr *header;
-	unsigned long count;
-	u32 image_size_sectors;
 	u32 image_offset_sectors;
+	u32 image_size_sectors;
+	unsigned long count;
 	u32 image_offset;
 	int ret;
-
-	header = spl_get_load_buffer(-sizeof(*header), stor_dev->blksz);
-	count = blk_dread(stor_dev, sector, 1, header);
-	if (count == 0)
-		return -EIO;
 
 	ret = spl_parse_image_header(spl_image, bootdev, header);
 	if (ret)
@@ -40,6 +35,7 @@ static int spl_sata_load_image_raw(struct spl_image_info *spl_image,
 	image_size_sectors = DIV_ROUND_UP(spl_image->size, stor_dev->blksz);
 	image_offset_sectors = spl_image->offset / stor_dev->blksz;
 	image_offset = spl_image->offset % stor_dev->blksz;
+
 	count = blk_dread(stor_dev, sector + image_offset_sectors,
 			image_size_sectors,
 			(void *)spl_image->load_addr);
@@ -50,6 +46,48 @@ static int spl_sata_load_image_raw(struct spl_image_info *spl_image,
 		memmove((void *)spl_image->load_addr,
 			(void *)spl_image->load_addr + image_offset,
 			spl_image->size);
+
+	return 0;
+}
+
+static ulong spl_sata_load_read(struct spl_load_info *load, ulong sector,
+				ulong count, void *buf)
+{
+	struct blk_desc *stor_dev = load->dev;
+
+	return blk_dread(stor_dev, sector, count, buf);
+}
+
+static int spl_sata_load_image_raw(struct spl_image_info *spl_image,
+				   struct spl_boot_device *bootdev,
+				   struct blk_desc *stor_dev, unsigned long sector)
+{
+	struct legacy_img_hdr *header;
+	unsigned long count;
+	int ret;
+
+	header = spl_get_load_buffer(-sizeof(*header), stor_dev->blksz);
+	count = blk_dread(stor_dev, sector, 1, header);
+	if (count == 0)
+		return -EIO;
+
+	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) &&
+	    image_get_magic(header) == FDT_MAGIC) {
+		struct spl_load_info load;
+
+		debug("Found FIT\n");
+		load.dev = stor_dev;
+		load.priv = NULL;
+		load.filename = NULL;
+		load.bl_len = stor_dev->blksz;
+		load.read = spl_sata_load_read;
+		ret = spl_load_simple_fit(spl_image, &load, sector, header);
+	} else {
+		ret = sata_load_legacy(spl_image, bootdev, stor_dev, sector, header);
+	}
+
+	if (ret)
+		return -1;
 
 	return 0;
 }
