6ec113f968d2f80074288db5a1dd38844440b6aa
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2024-06-20 15:51:13 +0800

    lib: hash-checksum: Add DM_HASH support
    Add DM_HASH support for hash_checksum(), which is used
    by FIT signature verification.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: I79381cbf3e31487d10621fc89035a852f12e53da

--- lib/hash-checksum.c
+++ lib/hash-checksum.c
@@ -9,21 +9,60 @@
 #include <asm/byteorder.h>
 #include <linux/errno.h>
 #include <asm/unaligned.h>
+#ifndef CONFIG_DM_HASH
 #include <hash.h>
 #else
+#include <dm.h>
+#include <u-boot/hash.h>
+#endif /* !CONFIG_DM_HASH */
+#else
 #include "fdt_host.h"
-#endif
 #include <hash.h>
+#endif	/* !USE_HOSTCC */
 #include <image.h>
 
 int hash_calculate(const char *name,
 		    const struct image_region *region,
 		    int region_count, uint8_t *checksum)
 {
-	struct hash_algo *algo;
 	int ret = 0;
 	void *ctx;
 	int i;
+
+#if !defined(USE_HOSTCC) && defined(CONFIG_DM_HASH)
+	enum HASH_ALGO algo;
+	struct udevice *dev;
+
+	if (region_count < 1)
+		return -EINVAL;
+
+	ret = uclass_get_device(UCLASS_HASH, 0, &dev);
+	if (ret) {
+		debug("failed to get hash device, rc=%d\n", ret);
+		return -ENODEV;
+	}
+
+	algo = hash_algo_lookup_by_name(name);
+	if (algo == HASH_ALGO_INVALID) {
+		debug("unsupported hash algorithm\n");
+		return -EINVAL;
+	}
+
+	ret = hash_init(dev, algo, &ctx);
+	if (ret)
+		return ret;
+
+	for (i = 0; i < region_count; i++) {
+		ret = hash_update(dev, ctx, region[i].data, region[i].size);
+		if (ret)
+			return ret;
+	}
+
+	ret = hash_finish(dev, ctx, checksum);
+	if (ret)
+		return ret;
+#else
+	struct hash_algo *algo;
 
 	if (region_count < 1)
 		return -EINVAL;
@@ -49,6 +88,7 @@ int hash_calculate(const char *name,
 	ret = algo->hash_finish(algo, ctx, checksum, algo->digest_size);
 	if (ret)
 		return ret;
+#endif
 
 	return 0;
 }
