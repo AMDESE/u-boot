45c9f51ac3f839fa689c65ce79e0a9041d211daa
Kevin Chen <kevin_chen@aspeedtech.com>
2024-06-26 19:21:02 +0800

    configs: evb-ast2600/ast2700: Specify loadaddr for bootm
    In our case, tftp for SSP binary and boot into kernel with original
    image in flash. Prevent to use the img_addr by tftp, specify loadaddr
    with bootm command.
    
    Change-Id: I6fd7a5cf681bb788f195be237d2277d9e10adb9e

--- include/configs/evb_ast2600.h
+++ include/configs/evb_ast2600.h
@@ -17,14 +17,14 @@
 #define CFG_EXTRA_ENV_SETTINGS \
 	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"bootspi=fdt addr 20160000 && fdt header get fitsize totalsize && " \
-	"cp.b 20160000 ${loadaddr} ${fitsize} && bootm; " \
+	"cp.b 20160000 ${loadaddr} ${fitsize} && bootm ${loadaddr}; " \
 	"echo Error loading kernel FIT image\0" \
 	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0"	\
 	"bootside=a\0"	\
 	"rootfs=rofs-a\0"	\
 	"setmmcargs=setenv bootargs ${bootargs} rootwait root=PARTLABEL=${rootfs}\0"	\
-	"boota=setenv bootpart 2; setenv rootfs rofs-a; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm; echo Error loading kernel FIT image\0"	\
-	"bootb=setenv bootpart 3; setenv rootfs rofs-b; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm; echo Error loading kernel FIT image\0"	\
+	"boota=setenv bootpart 2; setenv rootfs rofs-a; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm ${loadaddr}; echo Error loading kernel FIT image\0"	\
+	"bootb=setenv bootpart 3; setenv rootfs rofs-b; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm ${loadaddr}; echo Error loading kernel FIT image\0"	\
 	"bootmmc=if test \"${bootside}\" = \"b\"; then run bootb; run boota; else run boota; run bootb; fi\0"	\
 	"verify=yes\0"	\
 	""
