d5d00837c7f03b5890ea135a571a4fe7d806efcd
Cool Lee <cool_lee@aspeedtech.com>
2024-11-07 17:38:40 +0800

    drivers: mmc: Add tuning for HS200/SDR104 support
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: I02e69d4fb4c185bed9fce812408db3a278340e39

--- drivers/mmc/aspeed_sdhci.c
+++ drivers/mmc/aspeed_sdhci.c
@@ -10,15 +10,166 @@
 #include <dm.h>
 #include <dm/device_compat.h>
 #include <malloc.h>
+#include <mmc.h>
 #include <sdhci.h>
 #include <linux/err.h>
 #include <linux/delay.h>
 #include <dm/lists.h>
+#include <power/regulator.h>
+
+#define ASPEED_SDC_PHASE		0xf4
+#define   ASPEED_SDC_S1_PHASE_IN	GENMASK(25, 21)
+#define   ASPEED_SDC_S0_PHASE_IN	GENMASK(20, 16)
+#define   ASPEED_SDC_S0_PHASE_IN_SHIFT	16
+#define   ASPEED_SDC_S0_PHASE_OUT_SHIFT 3
+#define   ASPEED_SDC_S1_PHASE_OUT	GENMASK(15, 11)
+#define   ASPEED_SDC_S1_PHASE_IN_EN	BIT(10)
+#define   ASPEED_SDC_S1_PHASE_OUT_EN	GENMASK(9, 8)
+#define   ASPEED_SDC_S0_PHASE_OUT	GENMASK(7, 3)
+#define   ASPEED_SDC_S0_PHASE_IN_EN	BIT(2)
+#define   ASPEED_SDC_S0_PHASE_OUT_EN	GENMASK(1, 0)
+#define   ASPEED_SDC_PHASE_MAX		31
+
+#define ASPEED_SDHCI_TAP_PARAM_INVERT_CLK	BIT(4)
+#define ASPEED_SDHCI_NR_TAPS		15
 
 struct aspeed_sdhci_plat {
 	struct mmc_config cfg;
 	struct mmc mmc;
 };
+
+#ifdef MMC_SUPPORTS_TUNING
+static int aspeed_execute_tuning(struct mmc *mmc, u8 opcode)
+{
+	struct sdhci_host *host = mmc->priv;
+	u32 val, left, right, edge;
+	u32 window, oldwindow = 0, center;
+	u32 in_phase, out_phase, enable_mask, inverted = 0;
+
+	out_phase = sdhci_readl(host, ASPEED_SDC_PHASE) & ASPEED_SDC_S0_PHASE_OUT;
+
+	enable_mask = ASPEED_SDC_S0_PHASE_OUT_EN | ASPEED_SDC_S0_PHASE_IN_EN;
+
+	/*
+	 * There are two window upon clock rising and falling edge.
+	 * Iterate each tap delay to find the valid window and choose the
+	 * bigger one, set the tap delay at the middle of window.
+	 */
+	for (edge = 0; edge < 2; edge++) {
+		if (edge == 1)
+			inverted = ASPEED_SDHCI_TAP_PARAM_INVERT_CLK;
+
+		val = (out_phase | enable_mask | (inverted << ASPEED_SDC_S0_PHASE_IN_SHIFT));
+
+		/* find the left boundary */
+		for (left = 0; left < ASPEED_SDHCI_NR_TAPS + 1; left++) {
+			in_phase = val | (left << ASPEED_SDC_S0_PHASE_IN_SHIFT);
+			sdhci_writel(host, in_phase, ASPEED_SDC_PHASE);
+			if (!mmc_send_tuning(mmc, opcode, NULL))
+				break;
+		}
+
+		/* find the right boundary */
+		for (right = left + 1; right < ASPEED_SDHCI_NR_TAPS + 1; right++) {
+			in_phase = val | (right << ASPEED_SDC_S0_PHASE_IN_SHIFT);
+			sdhci_writel(host, in_phase, ASPEED_SDC_PHASE);
+			if (mmc_send_tuning(mmc, opcode, NULL))
+				break;
+		}
+
+		window = right - left;
+		pr_debug("tuning window[%d][%d~%d] = %d\n", edge, left, right, window);
+
+		if (window > oldwindow) {
+			oldwindow = window;
+			center = (((right - 1) + left) / 2) | inverted;
+		}
+	}
+
+	val = (out_phase | enable_mask | (center << ASPEED_SDC_S0_PHASE_IN_SHIFT));
+	sdhci_writel(host, val, ASPEED_SDC_PHASE);
+
+	pr_debug("input tuning result=%x\n", val);
+
+	inverted = 0;
+	out_phase = val & ~ASPEED_SDC_S0_PHASE_OUT;
+	in_phase = out_phase;
+	oldwindow = 0;
+
+	for (edge = 0; edge < 2; edge++) {
+		if (edge == 1)
+			inverted = ASPEED_SDHCI_TAP_PARAM_INVERT_CLK;
+
+		val = (in_phase | enable_mask | (inverted << ASPEED_SDC_S0_PHASE_OUT_SHIFT));
+
+		/* find the left boundary */
+		for (left = 0; left < ASPEED_SDHCI_NR_TAPS + 1; left++) {
+			out_phase = val | (left << ASPEED_SDC_S0_PHASE_OUT_SHIFT);
+			sdhci_writel(host, out_phase, ASPEED_SDC_PHASE);
+
+			if (!mmc_send_tuning(mmc, opcode, NULL))
+				break;
+		}
+
+		/* find the right boundary */
+		for (right = left + 1; right < ASPEED_SDHCI_NR_TAPS + 1; right++) {
+			out_phase = val | (right << ASPEED_SDC_S0_PHASE_OUT_SHIFT);
+			sdhci_writel(host, out_phase, ASPEED_SDC_PHASE);
+
+			if (mmc_send_tuning(mmc, opcode, NULL))
+				break;
+		}
+
+		window = right - left;
+		pr_debug("tuning window[%d][%d~%d] = %d\n", edge, left, right, window);
+
+		if (window > oldwindow) {
+			oldwindow = window;
+			center = (((right - 1) + left) / 2) | inverted;
+		}
+	}
+
+	val = (in_phase | enable_mask | (center << ASPEED_SDC_S0_PHASE_OUT_SHIFT));
+	sdhci_writel(host, val, ASPEED_SDC_PHASE);
+
+	pr_debug("output tuning result=%x\n", val);
+
+	return mmc_send_tuning(mmc, opcode, NULL);
+}
+
+static int aspeed_set_ios_post(struct sdhci_host *host)
+{
+	struct mmc *mmc = host->mmc;
+	u32 reg;
+	u32 drv;
+
+	drv = dev_read_u32_default(mmc->dev, "sdhci-drive-type", 0);
+
+	reg = sdhci_readw(host, SDHCI_HOST_CONTROL2);
+	reg &= ~SDHCI_CTRL_DRV_TYPE_MASK;
+
+	switch (mmc->selected_mode) {
+	case SD_HS:
+	case UHS_SDR50:
+	case UHS_DDR50:
+	case UHS_SDR104:
+		reg |= ((drv & 0x3) << 4);
+		break;
+	default:
+		break;
+	}
+
+	sdhci_writew(host, reg, SDHCI_HOST_CONTROL2);
+
+	return 0;
+}
+
+static struct sdhci_ops aspeed_sdhci_ops = {
+	.set_control_reg = sdhci_set_control_reg,
+	.set_ios_post = aspeed_set_ios_post,
+	.platform_execute_tuning = aspeed_execute_tuning,
+};
+#endif
 
 static int aspeed_sdhci_probe(struct udevice *dev)
 {
@@ -26,8 +177,11 @@ static int aspeed_sdhci_probe(struct udevice *dev)
 	struct aspeed_sdhci_plat *plat = dev_get_plat(dev);
 	struct sdhci_host *host = dev_get_priv(dev);
 	u32 max_clk;
+	u32 max_freq;
 	struct clk clk;
 	int ret;
+
+	max_freq = dev_read_u32_default(dev, "max-frequency", 4);
 
 	ret = clk_get_by_index(dev, 0, &clk);
 	if (ret) {
@@ -51,6 +205,9 @@ static int aspeed_sdhci_probe(struct udevice *dev)
 		goto err;
 	}
 
+#ifdef MMC_SUPPORTS_TUNING
+	host->ops = &aspeed_sdhci_ops;
+#endif
 	host->max_clk = max_clk;
 	host->mmc = &plat->mmc;
 	host->mmc->dev = dev;
@@ -62,7 +219,7 @@ static int aspeed_sdhci_probe(struct udevice *dev)
 	if (host->bus_width == 8)
 		host->host_caps |= MMC_MODE_8BIT;
 
-	ret = sdhci_setup_cfg(&plat->cfg, host, 0, 400000);
+	ret = sdhci_setup_cfg(&plat->cfg, host, max_freq, 400000);
 	if (ret)
 		goto err;
 
