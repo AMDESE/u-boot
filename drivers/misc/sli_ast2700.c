AMDESE/u-boot

commit 8a07c2596a79e9bf0699ac6a08a74ed56ec6e0cf

misc: sli_ast2700: Do not re-calibrate SLI
Add a flag in SCU1 scratch register #31 to indicate that both sides of
SLI delay had been calibrated, and therefore skip the remaining
procedures.

Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
Change-Id: Ifcb25f73ebb5c99d81fa8b53ae88fb63977452b8

drivers/misc/sli_ast2700.c
@@ -105,6 +105,11 @@ struct sli_data {
 #define SLIM_COARSE_D_END		16
 #define SLIM_FINE_MARGIN		5
 
+#define SCU1_SCRATCH31_SLI0_READY	BIT(0)
+#define SCU1_SCRATCH31_SLI_SKIP_CALI	BIT(1)	/* skip calibration */
+#define SCU0_SCRATCH31_SLI1_READY	BIT(0)
+#define AHBC_MAX_TIMEOUT		0x1ff
+
 static void sli_clear_interrupt_status(uintptr_t base)
 {
 	writel(0xfffff, (void *)base + SLI_INTR_STATUS);
@@ -473,10 +478,9 @@ int ast2700_sli1_probe(struct udevice *dev)
 		data->flags |= SLI_FLAG_AST2700A0;
 
 	/* Return if SLI had been calibrated */
-	reg_val = readl((void *)data->die1.slih + SLI_CTRL_III);
-	reg_val = FIELD_GET(SLI_CLK_SEL, reg_val);
-	if (reg_val) {
-		debug("SLI has been initialized\n");
+	reg_val = readl((void *)&scu->scratch[31]);
+	if (reg_val & SCU1_SCRATCH31_SLI_SKIP_CALI) {
+		debug("SLI1 has been initialized\n");
 		return 0;
 	}
 
@@ -548,10 +552,6 @@ int ast2700_sli1_probe(struct udevice *dev)
 	return 0;
 }
 
-#define SCU1_SCRATCH31_SLI0_READY	BIT(0)
-#define SCU0_SCRATCH31_SLI1_READY	BIT(0)
-#define AHBC_MAX_TIMEOUT		0x1ff
-
 int ast2700_sli0_probe(struct udevice *dev)
 {
 	struct ast2700_scu0 *scu0;
@@ -587,7 +587,13 @@ int ast2700_sli0_probe(struct udevice *dev)
 		return 0;
 
 	if (IS_ENABLED(CONFIG_SLI_TARGET_PHYCLK_25MHZ)) {
-		printf("AST2700 SLI0 ready, 25MHz\n");
+		debug("AST2700 SLI0 ready, 25MHz\n");
+		return 0;
+	}
+
+	reg_val = readl((void *)&scu1->scratch[31]);
+	if (reg_val & SCU1_SCRATCH31_SLI_SKIP_CALI) {
+		printf("SLI0 has been initialized\n");
 		return 0;
 	}
 
@@ -621,6 +627,9 @@ int ast2700_sli0_probe(struct udevice *dev)
 		writel(AHBC_MAX_TIMEOUT, (void *)ASPEED_AHBC0_BASE + 0x0b4);
 		writel(AHBC_MAX_TIMEOUT, (void *)ASPEED_AHBC0_BASE + 0x0f4);
 		debug("SLI0 calibration completed\n");
+
+		setbits_le32((void *)&scu1->scratch[31],
+			     SCU1_SCRATCH31_SLI_SKIP_CALI);
 		return 0;
 	}
 

commit 26082077d404adb13f3862b1acb9cc3ccb3a0f99

sli_ast2700: Fix reset issue on AST2700A0
There is no IO-die scratch31[1] on AST2700A0 so add additional check on
for AST2700A0 & A1 to avoid SLI re-calibration after warm reset.

Fixes: 8a07c2596a79 ("misc: sli_ast2700: Do not re-calibrate SLI")

Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
Change-Id: I27c5386f02962695c4cd965a7c091528f7a9a663

drivers/misc/sli_ast2700.c
@@ -477,15 +477,16 @@ int ast2700_sli1_probe(struct udevice *dev)
 	if (FIELD_GET(SCU_CPU_REVISION_ID_HW, reg_val) == 0)
 		data->flags |= SLI_FLAG_AST2700A0;
 
-	/* Return if SLI had been calibrated */
-	reg_val = readl((void *)&scu->scratch[31]);
-	if (reg_val & SCU1_SCRATCH31_SLI_SKIP_CALI) {
-		debug("SLI1 has been initialized\n");
-		return 0;
-	}
-
-	/* AST2700A0 workaround for 25MHz */
 	if (data->flags & SLI_FLAG_AST2700A0) {
+		/* Return if SLI had been calibrated */
+		reg_val = readl((void *)data->die1.slih + SLI_CTRL_III);
+		reg_val = FIELD_GET(SLI_CLK_SEL, reg_val);
+		if (reg_val) {
+			debug("SLI has been initialized\n");
+			return 0;
+		}
+
+		/* AST2700A0 workaround for 25MHz */
 		reg_val = SLI_RX_PHY_LAH_SEL_NEG | SLI_TRANS_EN | SLI_CLEAR_BUS;
 		writel(reg_val, (void *)data->die1.slih + SLI_CTRL_I);
 		writel(reg_val, (void *)data->die1.slim + SLI_CTRL_I);
@@ -501,24 +502,33 @@ int ast2700_sli1_probe(struct udevice *dev)
 		if (data->die0.phy_clk_freq == SLI_PHYCLK_800M ||
 		    data->die0.phy_clk_freq == SLI_PHYCLK_788M)
 			data->flags |= SLI_FLAG_RX_LAH_NEG_IO_SLIM;
+	} else {
+		/* Return if SLI had been calibrated */
+		reg_val = readl((void *)&scu->scratch[31]);
+		if (reg_val & SCU1_SCRATCH31_SLI_SKIP_CALI) {
+			debug("SLI1 has been initialized\n");
+			return 0;
+		}
 	}
 
 	if (IS_ENABLED(CONFIG_SLI_TARGET_PHYCLK_25MHZ))
 		return 0;
 
-	/* Disable AHBC timeout before calibration */
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x034);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x074);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x0b4);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x0f4);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x134);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x174);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x1b4);
-	writel(0, (void *)ASPEED_AHBC1_BASE + 0x1f4);
-	writel(0, (void *)ASPEED_AHBC0_BASE + 0x034);
-	writel(0, (void *)ASPEED_AHBC0_BASE + 0x074);
-	writel(0, (void *)ASPEED_AHBC0_BASE + 0x0b4);
-	writel(0, (void *)ASPEED_AHBC0_BASE + 0x0f4);
+	if (!(data->flags & SLI_FLAG_AST2700A0)) {
+		/* Disable AHBC timeout before calibration */
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x034);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x074);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x0b4);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x0f4);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x134);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x174);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x1b4);
+		writel(0, (void *)ASPEED_AHBC1_BASE + 0x1f4);
+		writel(0, (void *)ASPEED_AHBC0_BASE + 0x034);
+		writel(0, (void *)ASPEED_AHBC0_BASE + 0x074);
+		writel(0, (void *)ASPEED_AHBC0_BASE + 0x0b4);
+		writel(0, (void *)ASPEED_AHBC0_BASE + 0x0f4);
+	}
 
 	/* Speed up engine clock before adjusting PHY TX clock and delay */
 	reg_val = FIELD_PREP(SLI_CLK_SEL, data->die1.eng_clk_freq);