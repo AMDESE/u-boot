f71b0bf830634488f89fe124b849d57834aa0b79
Cool Lee <cool_lee@aspeedtech.com>
2025-03-07 18:07:51 +0800

    arch: arm: mach-aspeed: Add ufs boot
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: I33fdd84c0d1b27d424440e7b1250151b0fbf29e9

--- include/configs/evb_ast2700.h
+++ include/configs/evb_ast2700.h
@@ -25,6 +25,10 @@
 	"boota=setenv bootpart 2; setenv rootfs rofs-a; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm ${loadaddr}; echo Error loading kernel FIT image\0"	\
 	"bootb=setenv bootpart 3; setenv rootfs rofs-b; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm ${loadaddr}; echo Error loading kernel FIT image\0"	\
 	"bootmmc=if test \"${bootside}\" = \"b\"; then run bootb; run boota; else run boota; run bootb; fi\0"	\
+	"setufsargs=setenv bootargs ${bootargs} rootwait root=PARTLABEL=${rootfs}\0"	\
+	"ufsboota=setenv bootpart 2; setenv rootfs rofs-a; run setufsargs; ext4load scsi 0:${bootpart} ${loadaddr} fitImage && bootm ${loadaddr}; echo Error loading kernel FIT image\0"	\
+	"ufsbootb=setenv bootpart 3; setenv rootfs rofs-b; run setufsargs; ext4load scsi 0:${bootpart} ${loadaddr} fitImage && bootm ${loadaddr}; echo Error loading kernel FIT image\0"	\
+	"bootufs=if test \"${bootside}\" = \"b\"; then run ufsbootb; run ufsboota; else run ufsboota; run ufsbootb; fi\0"	\
 	"verify=yes\0"	\
 	""
 #endif	/* __CONFIG_H */
