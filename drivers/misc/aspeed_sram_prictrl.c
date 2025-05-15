d135a447f48d2dcfa5532b80b6c9bc61c2454bb4
Kevin Chen <kevin_chen@aspeedtech.com>
2025-05-15 19:11:45 +0800

    drivers: misc: Update SRAM privilege control with region setting
    Current read/write permission is for the RX and TX to access only.
    For example,
      region0(TX:SSP,  RX:NS-CA35): 0x1000_0000~0x1000_2000
      region1(TX:SSP,  RX: S-CA35): 0x1000_2000~0x1000_4000
      region2(TX:BMCU, RX: SSP):    0x1000_4000~0x1000_6000
      region3(TX:SSP,  RX: BMCU):   0x1000_6000~0x1000_8000
    
    Change-Id: Ic607ab6ed4b729e0d84ca3ae80e68aa34058578b
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- drivers/misc/aspeed_sram_prictrl.c
+++ drivers/misc/aspeed_sram_prictrl.c
@@ -35,36 +35,104 @@ static int esram_prictrl_init(uintptr_t ctrl_base, uintptr_t sprot_addr, uint32_
 	sprot_ctrl = (struct sprot_region_enable_ast2700 *)(ctrl_base + ESRAM_SPROT_CTL);
 	sprot_region = (struct sprot_addr_ast2700 *)(ctrl_base + ESRAM_SPROT_ADR);
 
-	/* (0x12C0E100)SPROT_SIDG0 = 0x0b04; */
-	sprot_sid_ctrl->sidg0.b.sid0 = 0x4; /* SSP */
-	sprot_sid_ctrl->sidg0.b.sid1 = 0xb; /* TSP */
+	/* (0x12C0E100)SPROT_SIDG0 = 0x81800100; */
+	sprot_sid_ctrl->sidg0.b.sid0 = 0x0; /* ARM_W */
+	sprot_sid_ctrl->sidg0.b.sid1 = 0x1; /* ARM_R */
+	sprot_sid_ctrl->sidg0.b.sid2 = 0x80; /* secure ARM_W */
+	sprot_sid_ctrl->sidg0.b.sid3 = 0x81; /* secure ARM_R */
 	debug("reg(sidg0) \t\taddr:0x%p,\tvalue:%x\n", &sprot_sid_ctrl->sidg0.raw,
 	      sprot_sid_ctrl->sidg0.raw);
 
-	/* region0 for SSP/TSP read write but others read only . */
-	sprot_ctrl->region0_enable.ctrl.b.w_enable_sid0 = 1; /* region0 enable write for SSP */
-	sprot_ctrl->region0_enable.ctrl.b.r_enable_sid0 = 1; /* region0 enable read for SSP */
-	sprot_ctrl->region0_enable.ctrl.b.w_enable_sid1 = 1; /* region0 enable write for TSP */
-	sprot_ctrl->region0_enable.ctrl.b.r_enable_sid1 = 1; /* region0 enable read for TSP */
-	sprot_ctrl->region0_enable.ctrl.b.r_enable_sid7 = 1; /* region0 enable read for Others */
+	/* (0x12C0E104)SPROT_SIDG0 = 0x210b0400; */
+	sprot_sid_ctrl->sidg1.b.sid4 = 0x4; /* SSP_S */
+	sprot_sid_ctrl->sidg1.b.sid5 = 0xb; /* TSP_S */
+	sprot_sid_ctrl->sidg1.b.sid6 = 0x21; /* MCU0D */
+	debug("reg(sidg1) \t\taddr:0x%p,\tvalue:%x\n", &sprot_sid_ctrl->sidg1.raw,
+	      sprot_sid_ctrl->sidg1.raw);
+
+	/*
+	 * (0x12C0E1c0)region0: start_address = 0x0, size = 0x800
+	 * Read/Write permission = SSP and ARM_R
+	 * Start address = 0x1000_0000 : 0x1000_0000(SRAM base) + 0x0(start address) * 4(unit size)
+	 * End address   = 0x1000_2000 : 0x1000_0000(Start address) + 0x800(size) * 4(unit size)
+	 */
+	sprot_ctrl->region0_enable.ctrl.b.w_enable_sid0 = 1; /* ARM_W */
+	sprot_ctrl->region0_enable.ctrl.b.r_enable_sid1 = 1; /* ARM_R */
+	sprot_ctrl->region0_enable.ctrl.b.w_enable_sid4 = 1; /* SSP_S */
+	sprot_ctrl->region0_enable.ctrl.b.r_enable_sid4 = 1; /* SSP_S */
 	debug("reg(region0_enable) \taddr:0x%p,\tvalue:%x\n", &sprot_ctrl->region0_enable.ctrl.raw,
 	      sprot_ctrl->region0_enable.ctrl.raw);
 
-	/*
-	 * (0x12C0E1c0)region0:
-	 * Start address : 0x1000_0000(SRAM base) + 0x0(start address) * 4(unit size)
-	 * End address   : 0x1000_0000(Start address) + sprot_size
-	 */
 	sprot_region->region0.b.start_address = 0x0;
-	if (sprot_size >= 0x40000)
-		sprot_region->region0.b.size = 0xffff; /* 0x1000_0000 + 0x3fffc in max setting */
-	else
-		sprot_region->region0.b.size = sprot_size >> 2; /* 0x1000_0000 + sprot_size */
+	sprot_region->region0.b.size = 0x800;
 	debug("reg(region0) \t\taddr:0x%p,\tvalue:%x\n", &sprot_region->region0.raw,
 	      sprot_region->region0.raw);
 	debug("\tProtect range \taddr:0x%lx, \tsize:%x\n",
 	      sprot_addr + sprot_region->region0.b.start_address * sprot_unit,
 	      sprot_region->region0.b.size * sprot_unit);
+
+	/*
+	 * (0x12C0E1c4)region1: start_address = 0x800, size = 0x800
+	 * Read/Write permission = SSP and secure ARM_R
+	 * Start address = 0x1000_2000 : 0x1000_0000(SRAM base) + 0x800(start address) * 4(unit size)
+	 * End address   = 0x1000_4000 : 0x1000_2000(Start address) + 0x800 * 4(unit size)
+	 */
+	sprot_ctrl->region1_enable.ctrl.b.w_enable_sid2 = 1; /* secure ARM_W */
+	sprot_ctrl->region1_enable.ctrl.b.r_enable_sid3 = 1; /* secure ARM_R */
+	sprot_ctrl->region1_enable.ctrl.b.w_enable_sid4 = 1; /* SSP_S */
+	sprot_ctrl->region1_enable.ctrl.b.r_enable_sid4 = 1; /* SSP_S */
+	debug("reg(region1_enable) \taddr:0x%p,\tvalue:%x\n", &sprot_ctrl->region1_enable.ctrl.raw,
+	      sprot_ctrl->region1_enable.ctrl.raw);
+
+	sprot_region->region1.b.start_address = 0x800;
+	sprot_region->region1.b.size = 0x800;
+	debug("reg(region1) \t\taddr:0x%p,\tvalue:%x\n", &sprot_region->region1.raw,
+	      sprot_region->region1.raw);
+	debug("\tProtect range \taddr:0x%lx, \tsize:%x\n",
+	      sprot_addr + sprot_region->region1.b.start_address * sprot_unit,
+	      sprot_region->region1.b.size * sprot_unit);
+
+	/*
+	 * (0x12C0E1c8)region2: start_address = 0x1000, size = 0x800
+	 * Read/Write permission = MCU0D and SSP
+	 * Start address = 0x1000_4000 : 0x1000_0000(SRAM base) + 0x1000(start address) * 4(unit size)
+	 * End address   = 0x1000_6000 : 0x1000_4000(Start address) + 0x800 * 4(unit size)
+	 */
+	sprot_ctrl->region2_enable.ctrl.b.w_enable_sid6 = 1; /* MCU0D */
+	sprot_ctrl->region2_enable.ctrl.b.r_enable_sid6 = 1; /* MCU0D */
+	sprot_ctrl->region2_enable.ctrl.b.w_enable_sid4 = 1; /* SSP */
+	sprot_ctrl->region2_enable.ctrl.b.r_enable_sid4 = 1; /* SSP */
+	debug("reg(region2_enable) \taddr:0x%p,\tvalue:%x\n", &sprot_ctrl->region2_enable.ctrl.raw,
+	      sprot_ctrl->region2_enable.ctrl.raw);
+
+	sprot_region->region2.b.start_address = 0x1000;
+	sprot_region->region2.b.size = 0x800;
+	debug("reg(region2) \t\taddr:0x%p,\tvalue:%x\n", &sprot_region->region2.raw,
+	      sprot_region->region2.raw);
+	debug("\tProtect range \taddr:0x%lx, \tsize:%x\n",
+	      sprot_addr + sprot_region->region2.b.start_address * sprot_unit,
+	      sprot_region->region2.b.size * sprot_unit);
+
+	/*
+	 * (0x12C0E1cc)region3: start_address = 0x1800, size = 0x800
+	 * Read/Write permission = MCU0D and SSP
+	 * Start address = 0x1000_6000 : 0x1000_0000(SRAM base) + 0x1800(start address) * 4(unit size)
+	 * End address   = 0x1000_8000 : 0x1000_6000(Start address) + 0x800 * 4(unit size)
+	 */
+	sprot_ctrl->region3_enable.ctrl.b.w_enable_sid6 = 1; /* MCU0D */
+	sprot_ctrl->region3_enable.ctrl.b.r_enable_sid6 = 1; /* MCU0D */
+	sprot_ctrl->region3_enable.ctrl.b.w_enable_sid4 = 1; /* SSP */
+	sprot_ctrl->region3_enable.ctrl.b.r_enable_sid4 = 1; /* SSP */
+	debug("reg(region3_enable) \taddr:0x%p,\tvalue:%x\n", &sprot_ctrl->region3_enable.ctrl.raw,
+	      sprot_ctrl->region3_enable.ctrl.raw);
+
+	sprot_region->region3.b.start_address = 0x1800;
+	sprot_region->region3.b.size = 0x800;
+	debug("reg(region3) \t\taddr:0x%p,\tvalue:%x\n", &sprot_region->region3.raw,
+	      sprot_region->region3.raw);
+	debug("\tProtect range \taddr:0x%lx, \tsize:%x\n",
+	      sprot_addr + sprot_region->region3.b.start_address * sprot_unit,
+	      sprot_region->region3.b.size * sprot_unit);
 
 	return ret;
 }
