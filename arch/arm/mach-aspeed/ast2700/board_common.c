c43cec1b83322d8f3d27adaf8d88f87a40bc3082
Jacky Chou <jacky_chou@aspeedtech.com>
2025-06-16 15:12:10 +0800

    arm: mach-aseed: add CONFIG for RGMII calibration
    Signed-off-by: Jacky Chou <jacky_chou@aspeedtech.com>
    Change-Id: I3093f80bdcbbb9233911994abd558a9099e4f041

--- arch/arm/mach-aspeed/ast2700/board_common.c
+++ arch/arm/mach-aspeed/ast2700/board_common.c
@@ -71,7 +71,8 @@ int board_init(void)
 	int ret;
 
 	ahbc_init();
-	aspeed_rgmii_init();
+	if (IS_ENABLED(CONFIG_AST2700_RGMII_CLIBRATION))
+		aspeed_rgmii_init();
 
 	regulators_enable_boot_on(0);
 
