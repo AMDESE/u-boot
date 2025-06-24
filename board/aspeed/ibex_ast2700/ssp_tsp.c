AMDESE/u-boot

commit 195e6691aa4bf396fc0cf689591671afab94508e

boards: ibex_ast2700: rename SSP and TSP control registers
Rename SSP and TSP control registers in the SCU header to align with the
datasheet.

Change-Id: I4f07aae0ff91c610fdbfc58740e39055e22fb891
Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>

board/aspeed/ibex_ast2700/ssp_tsp.c
@@ -62,7 +62,7 @@ int ssp_init(ulong load_addr)
 
 	scu = (struct ast2700_scu0 *)ASPEED_CPU_SCU_BASE;
 
-	reg_val = readl((void *)&scu->ssp_ctrl_1);
+	reg_val = readl((void *)&scu->ssp_ctrl_0);
 	if (!(reg_val & SCU_CPU_SSP_TSP_RESET_STS))
 		return 0;
 
@@ -71,7 +71,7 @@ int ssp_init(ulong load_addr)
 
 	reg_val = SCU_CPU_SSP_TSP_NIDEN | SCU_CPU_SSP_TSP_DBGEN |
 		  SCU_CPU_SSP_TSP_DBG_ENABLE | SCU_CPU_SSP_TSP_RESET;
-	writel(reg_val, (void *)&scu->ssp_ctrl_1);
+	writel(reg_val, (void *)&scu->ssp_ctrl_0);
 
 	/*
 	 * SSP Memory Map:
@@ -108,7 +108,7 @@ int ssp_init(ulong load_addr)
 	writel(TCM_SIZE, (void *)&scu->ssp_tcm_size);
 
 	/* Configure physical AHB remap: through H2M, mapped to SYS_DRAM_BASE */
-	writel((uint32_t)(0x400000000ULL >> 4), (void *)&scu->ssp_ctrl_2);
+	writel((uint32_t)(0x400000000ULL >> 4), (void *)&scu->ssp_ctrl_1);
 
 	/* Configure physical DRAM remap */
 	phy_addr = ((uint64_t)load_addr - ASPEED_DRAM_BASE) | 0x400000000ULL;
@@ -119,11 +119,12 @@ int ssp_init(ulong load_addr)
 	 * For A1, the Cache region can only be enabled entirely;
 	 * partial enabling is not supported.
 	 */
+	writel(GENMASK(31, 0), (void *)&scu->ssp_ctrl_3);
 	writel(GENMASK(31, 0), (void *)&scu->ssp_ctrl_4);
-	writel(GENMASK(31, 0), (void *)&scu->ssp_ctrl_5);
 
 	/* Enable I and D cache as default */
-	writel(SCU_CPU_SSP_CTR1_ICACHE_EN | SCU_CPU_SSP_CTR1_DCACHE_EN, (void *)&scu->ssp_ctrl_7);
+	writel(SCU_CPU_SSP_TSP_CTRL_ICACHE_EN | SCU_CPU_SSP_TSP_CTRL_DCACHE_EN,
+	       (void *)&scu->ssp_ctrl_6);
 
 	return 0;
 }
@@ -133,10 +134,10 @@ int ssp_enable(void)
 	struct ast2700_scu0 *scu;
 
 	scu = (struct ast2700_scu0 *)ASPEED_CPU_SCU_BASE;
-	setbits_le32((void *)&scu->ssp_ctrl_1, SCU_CPU_SSP_TSP_ENABLE);
+	setbits_le32((void *)&scu->ssp_ctrl_0, SCU_CPU_SSP_TSP_ENABLE);
 
 	/* HW auto de-asserts SSP reset when WDT timeout reset occurs */
-	clrbits_le32((void *)&scu->ssp_ctrl_1, SCU_CPU_SSP_TSP_RESET);
+	clrbits_le32((void *)&scu->ssp_ctrl_0, SCU_CPU_SSP_TSP_RESET);
 
 	return 0;
 }
@@ -157,7 +158,7 @@ int tsp_init(ulong load_addr)
 
 	scu = (struct ast2700_scu0 *)ASPEED_CPU_SCU_BASE;
 
-	reg_val = readl((void *)&scu->tsp_ctrl_1);
+	reg_val = readl((void *)&scu->tsp_ctrl_0);
 	if (!(reg_val & SCU_CPU_SSP_TSP_RESET_STS))
 		return 0;
 
@@ -166,7 +167,7 @@ int tsp_init(ulong load_addr)
 
 	reg_val = SCU_CPU_SSP_TSP_NIDEN | SCU_CPU_SSP_TSP_DBGEN |
 		  SCU_CPU_SSP_TSP_DBG_ENABLE | SCU_CPU_SSP_TSP_RESET;
-	writel(reg_val, (void *)&scu->tsp_ctrl_1);
+	writel(reg_val, (void *)&scu->tsp_ctrl_0);
 
 	/* TSP 0x0000_0000 - 0x0200_0000 -> DRAM */
 	writel(info.size, (void *)&scu->tsp_remap_size);
@@ -174,17 +175,18 @@ int tsp_init(ulong load_addr)
 	/* Configure physical DRAM remap */
 	phy_addr = ((uint64_t)info.base - 0x80000000) | 0x400000000ULL;
 	reg_val = (uint32_t)(phy_addr >> 4);
-	writel(reg_val, (void *)&scu->tsp_ctrl_3);
+	writel(reg_val, (void *)&scu->tsp_ctrl_1);
 
 	/*
 	 * For A1, the Cache region can only be enabled entirely;
 	 * partial enabling is not supported.
 	 */
-	writel(GENMASK(31, 0), (void *)&scu->tsp_ctrl_4);
-	writel(GENMASK(31, 0), (void *)&scu->tsp_ctrl_5);
+	writel(GENMASK(31, 0), (void *)&scu->tsp_ctrl_2);
+	writel(GENMASK(31, 0), (void *)&scu->tsp_ctrl_3);
 
 	/* Enable I and D cache as default */
-	writel(SCU_CPU_SSP_CTR1_ICACHE_EN | SCU_CPU_SSP_CTR1_DCACHE_EN, (void *)&scu->tsp_ctrl_7);
+	writel(SCU_CPU_SSP_TSP_CTRL_ICACHE_EN | SCU_CPU_SSP_TSP_CTRL_DCACHE_EN,
+	       (void *)&scu->tsp_ctrl_5);
 
 	return 0;
 }

arch/riscv/include/asm/arch-ast2700/scu_ast2700.h
@@ -69,8 +69,8 @@
 #define SCU_CPU_SSP_TSP_ENABLE			BIT(0)
 
 /* SSP control register 6 */
-#define SCU_CPU_SSP_CTR1_ICACHE_EN		BIT(1)
-#define SCU_CPU_SSP_CTR1_DCACHE_EN		BIT(0)
+#define SCU_CPU_SSP_TSP_CTRL_ICACHE_EN		BIT(1)
+#define SCU_CPU_SSP_TSP_CTRL_DCACHE_EN		BIT(0)
 
 /* SoC1 SCU Register */
 #define SCU_IO_HWSTRAP_UFS			BIT(23)
@@ -169,13 +169,13 @@ struct ast2700_scu0 {
 	uint32_t random_num_ctrl;	/* 0x0F0 */
 	uint32_t random_num_data;	/* 0x0F4 */
 	uint32_t rsv_0xF8[10];		/* 0x0F8 ~ 0x11C */
-	uint32_t ssp_ctrl_1;		/* 0x120 */
-	uint32_t ssp_ctrl_2;		/* 0x124 */
-	uint32_t ssp_ctrl_3;		/* 0x128 */
-	uint32_t ssp_ctrl_4;		/* 0x12C */
-	uint32_t ssp_ctrl_5;		/* 0x130 */
-	uint32_t ssp_ctrl_6;		/* 0x134 */
-	uint32_t ssp_ctrl_7;		/* 0x138 */
+	uint32_t ssp_ctrl_0;		/* 0x120 */
+	uint32_t ssp_ctrl_1;		/* 0x124 */
+	uint32_t ssp_ctrl_2;		/* 0x128 */
+	uint32_t ssp_ctrl_3;		/* 0x12C */
+	uint32_t ssp_ctrl_4;		/* 0x130 */
+	uint32_t ssp_ctrl_5;		/* 0x134 */
+	uint32_t ssp_ctrl_6;		/* 0x138 */
 	uint32_t rsv_0x13c[1];		/* 0x13C */
 	uint32_t ssp_tcm_base;		/* 0x140 */
 	uint32_t ssp_tcm_size;		/* 0x144 */
@@ -184,13 +184,13 @@ struct ast2700_scu0 {
 	uint32_t ssp_memory_base;	/* 0x150 */
 	uint32_t ssp_memory_size;	/* 0x154 */
 	uint32_t rsv_0x158[2];		/* 0x158 ~ 0x15C */
-	uint32_t tsp_ctrl_1;		/* 0x160 */
+	uint32_t tsp_ctrl_0;		/* 0x160 */
 	uint32_t rsv_0x164[1];		/* 0x164 */
-	uint32_t tsp_ctrl_3;		/* 0x168 */
-	uint32_t tsp_ctrl_4;		/* 0x16C */
-	uint32_t tsp_ctrl_5;		/* 0x170 */
-	uint32_t tsp_ctrl_6;		/* 0x174 */
-	uint32_t tsp_ctrl_7;		/* 0x178 */
+	uint32_t tsp_ctrl_1;		/* 0x168 */
+	uint32_t tsp_ctrl_2;		/* 0x16C */
+	uint32_t tsp_ctrl_3;		/* 0x170 */
+	uint32_t tsp_ctrl_4;		/* 0x174 */
+	uint32_t tsp_ctrl_5;		/* 0x178 */
 	uint32_t rsv_0x17c[6];		/* 0x17C ~ 0x190 */
 	uint32_t tsp_remap_size;	/* 0x194 */
 	uint32_t rsv_0x198[26];		/* 0x198 ~ 0x1FC */

commit 340c174e50b2b70e534ae638044e884d0b7ba5e5

boards: ibex_ast2700: Fix incorrect register access
The access to register offset 0x128 was not updated after changes were
made to the structure in scu_ast2700.h.

Fixes: 195e6691aa4b ("boards: ibex_ast2700: rename SSP and TSP control registers")
Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
Change-Id: I8f5f7015c46956d604e8167f4ac91a9d7dc40886

board/aspeed/ibex_ast2700/ssp_tsp.c
@@ -113,7 +113,7 @@ int ssp_init(ulong load_addr)
 	/* Configure physical DRAM remap */
 	phy_addr = ((uint64_t)load_addr - ASPEED_DRAM_BASE) | 0x400000000ULL;
 	reg_val = (uint32_t)(phy_addr >> 4);
-	writel(reg_val, (void *)&scu->ssp_ctrl_3);
+	writel(reg_val, (void *)&scu->ssp_ctrl_2);
 
 	/*
 	 * For A1, the Cache region can only be enabled entirely;
@@ -193,7 +193,7 @@ int tsp_enable(void)
 	struct ast2700_scu0 *scu;
 
 	scu = (struct ast2700_scu0 *)ASPEED_CPU_SCU_BASE;
-	setbits_le32((void *)&scu->tsp_ctrl_1, SCU_CPU_SSP_TSP_ENABLE | SCU_CPU_SSP_TSP_RESET);
+	setbits_le32((void *)&scu->tsp_ctrl_0, SCU_CPU_SSP_TSP_ENABLE | SCU_CPU_SSP_TSP_RESET);
 
 	return 0;
 }