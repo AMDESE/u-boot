b4bf79d953bf9e2efedbbccd235f0942e210c734
Jammy Huang <jammy_huang@aspeedtech.com>
2025-08-05 09:44:52 +0800

    misc: aspeed_vga: Fix VGA image water ripple
    Modify vga-packer's frame config to original value.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Change-Id: I7bc1243cbd8a2788c2a37ed1c96c9bae12ae0f3d

--- drivers/misc/aspeed_vga.c
+++ drivers/misc/aspeed_vga.c
@@ -197,7 +197,7 @@ static int ast_vga_probe(struct udevice *dev)
 		writel(0x00030009, priv->vgal_cpu_base + 0x10);
 		val = 0x10000000 | dac_src;
 		writel(val, priv->vgal_cpu_base + 0x50);
-		writel(0x00100000, priv->vgal_cpu_base + 0x44);
+		writel(0x00100010, priv->vgal_cpu_base + 0x44);
 		writel(0x00030009, priv->vgal_cpu_base + 0x110);
 		writel(0x00030009, priv->vgal_io_base + 0x10);
 		writel(0x00010002, priv->vgal_io_base + 0x44);
