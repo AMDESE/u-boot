a47f773db9dbe532ed15f03b1a3838a9f502a2f8
Jacky Chou <jacky_chou@aspeedtech.com>
2025-09-25 11:25:42 +0800

    net: aspeed_mdio: add cl45 support
    Add cluster 45 command
    
    Signed-off-by: Jacky Chou <jacky_chou@aspeedtech.com>
    Change-Id: Ieae5706175cd6fe7af82bb2a1a6e94f204bd8c31

--- drivers/net/aspeed_mdio.c
+++ drivers/net/aspeed_mdio.c
@@ -26,6 +26,10 @@
 #define   ASPEED_MDIO_CTRL_OP		GENMASK(27, 26)
 #define     MDIO_C22_OP_WRITE		0b01
 #define     MDIO_C22_OP_READ		0b10
+#define     MDIO_C45_OP_ADDR		0b00
+#define     MDIO_C45_OP_WRITE		0b01
+#define     MDIO_C45_OP_PREAD		0b10
+#define     MDIO_C45_OP_READ		0b11
 #define   ASPEED_MDIO_CTRL_PHYAD	GENMASK(25, 21)
 #define   ASPEED_MDIO_CTRL_REGAD	GENMASK(20, 16)
 #define   ASPEED_MDIO_CTRL_MIIWDATA	GENMASK(15, 0)
@@ -43,54 +47,107 @@ struct aspeed_mdio_priv {
 	void *base;
 };
 
-static int aspeed_mdio_read(struct udevice *mdio_dev, int addr, int devad, int reg)
+static int aspeed_mdio_op(struct udevice *mdio_dev, u8 st, u8 op, u8 phyad, u8 regad, u16 data)
 {
 	struct aspeed_mdio_priv *priv = dev_get_priv(mdio_dev);
 	u32 ctrl;
-	u32 data;
-	int rc;
-
-	if (devad != MDIO_DEVAD_NONE)
-		return -EOPNOTSUPP;
 
 	ctrl = ASPEED_MDIO_CTRL_FIRE
-		| FIELD_PREP(ASPEED_MDIO_CTRL_ST, ASPEED_MDIO_CTRL_ST_C22)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_OP, MDIO_C22_OP_READ)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_PHYAD, addr)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_REGAD, reg);
+		| FIELD_PREP(ASPEED_MDIO_CTRL_ST, st)
+		| FIELD_PREP(ASPEED_MDIO_CTRL_OP, op)
+		| FIELD_PREP(ASPEED_MDIO_CTRL_PHYAD, phyad)
+		| FIELD_PREP(ASPEED_MDIO_CTRL_REGAD, regad)
+		| FIELD_PREP(ASPEED_MDIO_CTRL_MIIWDATA, data);
 
 	writel(ctrl, priv->base + ASPEED_MDIO_CTRL);
+	/* Add dummy read to ensure triggering mdio controller */
+	(void)readl(priv->base + ASPEED_MDIO_CTRL);
+
+	return readl_poll_timeout(priv->base + ASPEED_MDIO_CTRL, ctrl,
+				  !(ctrl & ASPEED_MDIO_CTRL_FIRE),
+				  ASPEED_MDIO_TIMEOUT_US);
+}
+
+static int aspeed_mdio_get_data(struct udevice *mdio_dev)
+{
+	struct aspeed_mdio_priv *priv = dev_get_priv(mdio_dev);
+	u32 data;
+	int rc;
 
 	rc = readl_poll_timeout(priv->base + ASPEED_MDIO_DATA, data,
 				data & ASPEED_MDIO_DATA_IDLE,
 				ASPEED_MDIO_TIMEOUT_US);
-
 	if (rc < 0)
 		return rc;
 
 	return FIELD_GET(ASPEED_MDIO_DATA_MIIRDATA, data);
 }
 
+static int aspeed_mdio_read_c22(struct udevice *mdio_dev, int addr, int regnum)
+{
+	int rc;
+
+	rc = aspeed_mdio_op(mdio_dev, ASPEED_MDIO_CTRL_ST_C22, MDIO_C22_OP_READ,
+			    addr, regnum, 0);
+	if (rc < 0)
+		return rc;
+
+	return aspeed_mdio_get_data(mdio_dev);
+}
+
+static int aspeed_mdio_write_c22(struct udevice *mdio_dev, int addr, int regnum,
+				 u16 val)
+{
+	return aspeed_mdio_op(mdio_dev, ASPEED_MDIO_CTRL_ST_C22, MDIO_C22_OP_WRITE,
+			      addr, regnum, val);
+}
+
+static int aspeed_mdio_read_c45(struct udevice *mdio_dev, int addr, int devad,
+				int regnum)
+{
+	int rc;
+
+	rc = aspeed_mdio_op(mdio_dev, ASPEED_MDIO_CTRL_ST_C45, MDIO_C45_OP_ADDR,
+			    addr, devad, regnum);
+	if (rc < 0)
+		return rc;
+
+	rc = aspeed_mdio_op(mdio_dev, ASPEED_MDIO_CTRL_ST_C45, MDIO_C45_OP_READ,
+			    addr, devad, 0);
+	if (rc < 0)
+		return rc;
+
+	return aspeed_mdio_get_data(mdio_dev);
+}
+
+static int aspeed_mdio_write_c45(struct udevice *mdio_dev, int addr, int devad,
+				 int regnum, u16 val)
+{
+	int rc;
+
+	rc = aspeed_mdio_op(mdio_dev, ASPEED_MDIO_CTRL_ST_C45, MDIO_C45_OP_ADDR,
+			    addr, devad, regnum);
+	if (rc < 0)
+		return rc;
+
+	return aspeed_mdio_op(mdio_dev, ASPEED_MDIO_CTRL_ST_C45, MDIO_C45_OP_WRITE,
+			      addr, devad, val);
+}
+
+static int aspeed_mdio_read(struct udevice *mdio_dev, int addr, int devad, int reg)
+{
+	if (devad == MDIO_DEVAD_NONE)
+		return aspeed_mdio_read_c22(mdio_dev, addr, reg);
+
+	return aspeed_mdio_read_c45(mdio_dev, addr, devad, reg);
+}
+
 static int aspeed_mdio_write(struct udevice *mdio_dev, int addr, int devad, int reg, u16 val)
 {
-	struct aspeed_mdio_priv *priv = dev_get_priv(mdio_dev);
-	u32 ctrl;
+	if (devad == MDIO_DEVAD_NONE)
+		return aspeed_mdio_write_c22(mdio_dev, addr, reg, val);
 
-	if (devad != MDIO_DEVAD_NONE)
-		return -EOPNOTSUPP;
-
-	ctrl = ASPEED_MDIO_CTRL_FIRE
-		| FIELD_PREP(ASPEED_MDIO_CTRL_ST, ASPEED_MDIO_CTRL_ST_C22)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_OP, MDIO_C22_OP_WRITE)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_PHYAD, addr)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_REGAD, reg)
-		| FIELD_PREP(ASPEED_MDIO_CTRL_MIIWDATA, val);
-
-	writel(ctrl, priv->base + ASPEED_MDIO_CTRL);
-
-	return readl_poll_timeout(priv->base + ASPEED_MDIO_CTRL, ctrl,
-				  !(ctrl & ASPEED_MDIO_CTRL_FIRE),
-				  ASPEED_MDIO_TIMEOUT_US);
+	return aspeed_mdio_write_c45(mdio_dev, addr, devad, reg, val);
 }
 
 static const struct mdio_ops aspeed_mdio_ops = {
