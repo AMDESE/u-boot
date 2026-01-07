0e587987651af272b9ccbdf6da187c26f8bab7a7
Jacky Chou <jacky_chou@aspeedtech.com>
2026-01-07 14:07:25 +0800

    phy: aspeed: refactor driver
    Split nway and fixed-link
    
    Signed-off-by: Jacky Chou <jacky_chou@aspeedtech.com>
    Change-Id: I648f87993c9b7b3e7074a55853ca869b5219d0ae

--- drivers/phy/aspeed/phy-aspeed-sgmii.c
+++ drivers/phy/aspeed/phy-aspeed-sgmii.c
@@ -8,7 +8,6 @@
 #include <asm/io.h>
 #include <dm.h>
 #include <dm/device_compat.h>
-#include <errno.h>
 #include <generic-phy.h>
 #include <regmap.h>
 #include <soc.h>
@@ -18,23 +17,32 @@
 #include <linux/bitfield.h>
 
 #define SGMII_CFG			0x00
+#define   SGMII_CFG_FIFO_MODE			BIT(0)
+#define   SGMII_CFG_SPEED_SEL_MASK		GENMASK(5, 4)
+#define   SGMII_CFG_SPEED_SEL(x)		FIELD_PREP(SGMII_CFG_SPEED_SEL_MASK, (x))
+#define   SGMII_CFG_PWR_DOWN			BIT(11)
+#define   SGMII_CFG_AN_ENABLE			BIT(12)
+#define  SGMII_CFG_SW_RESET			BIT(15)
 #define SGMII_LINK_TIMER		0x08
 #define SGMII_NWAY_ACK			0x0c
 #define SGMII_PHY_CFG1			0x18
+#define   SGMII_PHY_SPEED_MASK			GENMASK(5, 4)
+#define   SGMII_PHY_SPEED(x)			FIELD_PREP(SGMII_PHY_SPEED_MASK, (x))
 #define SGMII_PHY_PIPE_CTL		0x20
+#define   SGMII_PCTL_TX_NO_DEEMPH		BIT(7)
 #define SGMII_FIFO_DELAY_THREHOLD	0x28
 #define SGMII_MODE			0x30
+#define   SGMII_MODE_ENABLE			BIT(0)
+#define   SGMII_MODE_USE_LOCAL_CONFIG		BIT(2)
 
-#define SGMII_CFG_FIFO_MODE		BIT(0)
-#define SGMII_CFG_SPEED_10M		0
-#define SGMII_CFG_SPEED_100M		BIT(4)
-#define SGMII_CFG_SPEED_1G		BIT(5)
-#define SGMII_CFG_PWR_DOWN		BIT(11)
-#define SGMII_CFG_AN_ENABLE		BIT(12)
-#define SGMII_CFG_SW_RESET		BIT(15)
-#define SGMII_PCTL_TX_NO_DEEMPH		BIT(7)
-#define SGMII_MODE_ENABLE		BIT(0)
-#define SGMII_MODE_USE_LOCAL_CONFIG	BIT(2)
+#define PCIEPHY_CLK			0x268
+#define   PCIEPHY_CLK_FREQ_MULTI_MASK		GENMASK(7, 0)
+#define   PCIEPHY_CLK_FREQ_MULTI(x)		FIELD_PREP(PCIEPHY_CLK_FREQ_MULTI_MASK, (x))
+#define   PCIEPHY_CLK_SEL_INTERNAL_25M		BIT(8)
+
+#define SGMII_SPEED_10M		0x00
+#define SGMII_SPEED_100M	0x01
+#define SGMII_SPEED_1G		0x02
 
 struct aspeed_sgmii {
 	phys_addr_t regs;
@@ -43,70 +51,79 @@ struct aspeed_sgmii {
 	struct phy pcie_phy;
 };
 
-static int aspeed_sgmii_phy_init(struct phy *phy)
+static int aspeed_sgmii_conf(struct phy *phy, bool nway, int speed)
 {
 	struct udevice *dev = phy->dev;
 	struct aspeed_sgmii *sgmii = dev_get_priv(dev);
-	int ret;
-
-	ret = generic_phy_init(&sgmii->pcie_phy);
-	if (ret)
-		return ret;
+	u32 cfg;
 
 	writel(0, sgmii->regs + SGMII_MODE);
+
 	writel(0, sgmii->regs + SGMII_CFG);
 	writel(SGMII_CFG_SW_RESET | SGMII_CFG_PWR_DOWN, sgmii->regs + SGMII_CFG);
-	writel(SGMII_CFG_AN_ENABLE, sgmii->regs + SGMII_CFG);
+	if (nway) {
+		writel(SGMII_CFG_AN_ENABLE, sgmii->regs + SGMII_CFG);
+	} else {
+		switch (speed) {
+		case 10:
+			cfg = SGMII_SPEED_10M;
+			break;
+		case 100:
+			cfg = SGMII_SPEED_100M;
+			break;
+		case 1000:
+			cfg = SGMII_SPEED_1G;
+			break;
+		default:
+			return -EINVAL;
+		}
+		writel(SGMII_PHY_SPEED(cfg), sgmii->regs + SGMII_PHY_CFG1);
+		writel(SGMII_CFG_SPEED_SEL(cfg), sgmii->regs + SGMII_CFG);
+	}
+
 	writel(0x0c, sgmii->regs + SGMII_FIFO_DELAY_THREHOLD);
 	writel(SGMII_PCTL_TX_NO_DEEMPH, sgmii->regs + SGMII_PHY_PIPE_CTL);
-	/* Set link timer for Nway state change */
+
+	/* Set link timer for state change */
 	writel(0x100, sgmii->regs + SGMII_LINK_TIMER);
+
 	/* Bit 0 always sets to 1 in ACK message */
 	writel(0x1, sgmii->regs + SGMII_NWAY_ACK);
-	writel(SGMII_MODE_ENABLE, sgmii->regs + SGMII_MODE);
+
+	cfg = SGMII_MODE_ENABLE;
+	if (!nway)
+		cfg |= SGMII_MODE_USE_LOCAL_CONFIG;
+	writel(cfg, sgmii->regs + SGMII_MODE);
 
 	return 0;
 }
 
-int aspeed_sgmii_set_speed(struct phy *phy, int speed)
+static int aspeed_sgmii_phy_init(struct phy *phy)
+{
+	/* Default to enable Nway, not need configure speed */
+	return aspeed_sgmii_conf(phy, true, 0);
+}
+
+int aspeed_sgmii_phy_set_speed(struct phy *phy, int speed)
+{
+	return aspeed_sgmii_conf(phy, false, speed);
+}
+
+int aspeed_sgmii_phy_exit(struct phy *phy)
 {
 	struct udevice *dev = phy->dev;
 	struct aspeed_sgmii *sgmii = dev_get_priv(dev);
-	u32 reg;
 
-	switch (speed) {
-	case 10:
-		reg = SGMII_CFG_SPEED_10M;
-		break;
-	case 100:
-		reg = SGMII_CFG_SPEED_100M;
-		break;
-	case 1000:
-		reg = SGMII_CFG_SPEED_1G;
-		break;
-	default:
-		return -EINVAL;
-	}
-
+	/* Disable SGMII controller */
 	writel(0, sgmii->regs + SGMII_MODE);
-	writel((reg >> 2), sgmii->regs + SGMII_PHY_CFG1);
-	writel(0, sgmii->regs + SGMII_CFG);
-	writel(SGMII_CFG_SW_RESET | SGMII_CFG_PWR_DOWN, sgmii->regs + SGMII_CFG);
-	writel(reg, sgmii->regs + SGMII_CFG);
-	writel(0x0c, sgmii->regs + SGMII_FIFO_DELAY_THREHOLD);
-	writel(SGMII_PCTL_TX_NO_DEEMPH, sgmii->regs + SGMII_PHY_PIPE_CTL);
-	/* Set link timer for Nway state change */
-	writel(0x100, sgmii->regs + SGMII_LINK_TIMER);
-	/* Bit 0 always sets to 1 in ACK message */
-	writel(0x1, sgmii->regs + SGMII_NWAY_ACK);
-	writel(SGMII_MODE_ENABLE | SGMII_MODE_USE_LOCAL_CONFIG, sgmii->regs + SGMII_MODE);
 
 	return 0;
 }
 
 struct phy_ops aspeed_sgmii_phy_ops = {
 	.init = aspeed_sgmii_phy_init,
-	.set_speed = aspeed_sgmii_set_speed,
+	.set_speed = aspeed_sgmii_phy_set_speed,
+	.exit = aspeed_sgmii_phy_exit,
 };
 
 int aspeed_sgmii_probe(struct udevice *dev)
@@ -135,7 +152,7 @@ static const struct udevice_id aspeed_sgmii_ids[] = {
 };
 
 U_BOOT_DRIVER(phy_aspeed_sgmii) = {
-	.name = "phy_aspeed_sgmii",
+	.name = "aspeed-sgmii",
 	.id = UCLASS_PHY,
 	.of_match = aspeed_sgmii_ids,
 	.probe = aspeed_sgmii_probe,
